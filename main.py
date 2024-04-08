af1e9518931667a745eeb4a361e96b221ee0c33b
kockon <timofejcesakov@gmail.com>
2024-04-09 00:26:04 +0300

    Добавил возможность читать вопросы

--- main.py
+++ main.py
@@ -38,9 +38,32 @@ def login():
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
-@app.route('/quoestion/<int:id>')
-def open_question():
-    return render_template('question.html')
+@app.route('/question/<int:id>', methods=['GET', 'POST'])
+def open_question(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('question.html',title=f'Мыло {form.title.data}', form=form)
 
 
 @app.route('/logout')
