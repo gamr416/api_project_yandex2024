2986b3ddb7fde1746e34a35c0619cbe92cd635a0
LineChangerr <dim.shvalev@gmail.com>
2024-04-10 17:15:02 +0300

    добавлена загрузка фото???

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask import render_template, redirect, request, abort
+from flask import render_template, redirect, request, abort, flash, url_for
 from data import db_session
 from forms.user import RegisterForm, LoginForm
 from forms.news import NewsForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-
+from werkzeug.utils import secure_filename
+import os
 import datetime
 
 
@@ -14,9 +15,16 @@ from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+UPLOAD_FOLDER = '/uploaded_images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -76,6 +84,21 @@ def logout():
 @app.route('/user')
 @login_required
 def open_user():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
     return render_template('user_info.html')
 
 # @app.route('/ask')
