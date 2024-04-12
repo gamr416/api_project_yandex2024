2d43eac56cf241abcc37ade3c65cdef72d187e1d
kockon <timofejcesakov@gmail.com>
2024-04-12 18:17:05 +0300

    Красивая кнопочка

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def success():
     if request.method == 'POST':
         f = request.files['file']
         f.filename = f"avatar_{current_user.id}.png"
-        avatar = f'static/uploaded_files/{f.filename}'
+        avatar = f'static/img/uploaded_files/{f.filename}'
         f.save(avatar)
         con = sqlite3.connect('db/blogs.db')
         cur = con.cursor()
