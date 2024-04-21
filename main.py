607d69ab132f34c6493075afcc223473d9b2fa65
kockon <timofejcesakov@gmail.com>
2024-04-21 18:17:44 +0300

    Попытка поправить что нибудь

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def index():
     db_sess = db_session.create_session()
     if current_user.is_authenticated:
         news = db_sess.query(News).filter(
-            (News.user == current_user) | (News.is_private != True))
+            (News.user != current_user) & (News.is_private != True))
     else:
         news = db_sess.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
