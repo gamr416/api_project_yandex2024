f4aa46fcf5557a82ffa59e8b58625d40a551b3a4
kockon <timofejcesakov@gmail.com>
2024-04-26 18:57:04 +0300

    Фронтенд

--- main.py
+++ main.py
@@ -126,36 +126,21 @@ def logout():
     return redirect("/")
 
 
-@app.route('/user')
+@app.route('/user', methods=['GET', 'POST'])
 @login_required
 def open_user():
-    if request.method == 'GET':
-        db_sess = db_session.create_session()
-        users_questions = db_sess.query(News).filter(News.user_id == current_user.id)
+    db_sess = db_session.create_session()
+    users_questions = db_sess.query(News).filter(News.user_id == current_user.id)
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file = request.files['file']
-            file.filename = f"avatar_{current_user.id}.png"
-            avatar = f'static/img/uploaded_files/{file.filename}'
-            file.save(avatar)
-            con = sqlite3.connect('db/blogs.db')
-            cur = con.cursor()
-            cur.execute(f'''UPDATE users SET avatar = "{avatar}" WHERE id = "{current_user.id}"''')
-            con.commit()
-            cur.close()
-        return redirect(url_for('download_file', name=filename))
+        file.filename = f"avatar_{current_user.id}.png"
+        avatar = f'static/img/uploaded_files/{file.filename}'
+        file.save(avatar)
+        con = sqlite3.connect('db/blogs.db')
+        cur = con.cursor()
+        cur.execute(f'''UPDATE users SET avatar = "{avatar}" WHERE id = "{current_user.id}"''')
+        con.commit()
+        cur.close()
     return render_template('user_info.html',
                            title=f'Мыло: {current_user.name}', questions=users_questions)
 
