8833d311b64b9898c9fa249d9fded5a6a08405c9
kockon <timofejcesakov@gmail.com>
2024-05-01 13:29:46 +0300

    фиксы по просьбе дорогих пользователей

--- data/answers.py
+++ data/answers.py
@@ -1,7 +1,6 @@
-import datetime
 import sqlalchemy
 from sqlalchemy import orm
-from .db_session import SqlAlchemyBase
+from db_session import SqlAlchemyBase
 
 
 class Answers(SqlAlchemyBase):
@@ -10,6 +9,5 @@ class Answers(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-    # news_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("news.id"))
     question_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=False, autoincrement=True)
     user = orm.relationship('User')
